c43438c5e9781680aa0d07461106b3bdaf71a24e
asonste <anders.sonsterud+github@gmail.com>
2016-06-30 01:49:52 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,14 +1,14 @@
 #/usr/bin/python
 """
-asonste 28/June.2016
+asonste 30/June.2016
 The script is called from crontab every minute
 To add crontab with output to debug file:
 sudo su
 crontab -e
 * * * * * /usr/bin/python /home/pi/Home-Watchdog/run.py >>/tmp/out.txt 2>&1
 """
+from conf import *
 from log import *
-#log("Test") # For verifying that the script is beeing run from crontab.
 
 from extip import *
 checkip()    # Check if external IP has changed 
@@ -18,3 +18,5 @@ check_locip()# Check if local IP has changed
 
 from aliveping import *
 runpingweb() # Writes Connected / Disconnected to .conf file and logs events 
+
+write_config('defult','Last-Alive-Time',dateandtime()) #write time to .conf file
