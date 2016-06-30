b846da61f14d2574ce028d791b913748447947e6
asonste <anders.sonsterud+github@gmail.com>
2016-06-30 14:39:25 +0200

    Update log.py

--- log.py
+++ log.py
@@ -3,11 +3,12 @@
 asonste 30/June.2016
 For loging events to a log file, and adding timestamp on the event
 One mandatory parameter (text) and one optional parameter (logfilepath).
-Latest changes: Function for returning dateandtime.
+Latest changes: Check if logging is allowed
 """
 from conf import *
 base = config.get('defult','base_dir')# Get defult logfilepath from .conf file
 path = ('%s/log.txt'%(base))
+allow_loging = config.get('defult','allow_loging')
 from time import strftime
 import time
 datetime = (time.strftime("%Y-%m-%d ") + time.strftime("%H:%M:%S"))
@@ -16,9 +17,10 @@ def dateandtime():
    return str(datetime)
  
 def log(string,logfilepath=path):
-   f = open(logfilepath, 'a') # a for append, w for write.
-   f.write(datetime + ' {0}\n'.format(string))
-   f.close() # Close the file
+   if allow_loging == "1":
+      f = open(logfilepath, 'a') # a for append, w for write.
+      f.write(datetime + ' {0}\n'.format(string))
+      f.close() # Close the file
 
 #--- For testing -------------
 if __name__ == "__main__":
